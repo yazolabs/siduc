0bc8b23fa2e1aad9f2b25e995444148586931029
Leo <leonardobsantos_@hotmail.com>
2026-01-03 16:58:03 -0300

    ajuste Dockerfile.prod

--- public/config/prematricula.js
+++ public/config/prematricula.js
@@ -1,1 +1,25 @@
-window.prematricula = window.prematricula || {};
+
+(function () {
+  window.config = window.config || {};
+
+  window.config.city = window.config.city ?? "Goiana";
+  window.config.state = window.config.state ?? "PE";
+
+  window.config.ibge_codes = window.config.ibge_codes ?? "2606200";
+
+  window.config.logo = window.config.logo ?? null;
+  window.config.slogan = window.config.slogan ?? "";
+
+  window.config.map = window.config.map || {
+    lat: -7.560556,
+    lng: -35.0025,
+    zoom: 12,
+  };
+
+  window.config.video_intro_url =
+    window.config.video_intro_url ??
+    "https://www.youtube.com/embed/ltXDgjS-XpA?html5=1";
+
+  window.config.token =
+    window.config.token ?? "O1oS2c032JHxnclkS7kpfKfsi2ey7DLT";
+})();
